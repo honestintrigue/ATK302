d341a8f64d4825799ded6cb88aa98ed88a8fe1fd
Brooke <BAMAST1@ilstu.edu>
2019-10-03 15:40:17 -0700

    assignment2boogie

--- p5/mp2/sketch.js
+++ p5/mp2/sketch.js
@@ -1,46 +1,71 @@
 var myState=0;
-var timer=0;
-var gif_loadImg, gif_createImg;
+var myTimer=0;
+var B1,B2;
+var y=-10;
 
-function preload() {
-  gif_loadImg = loadImage("assets/gifboogie.gif");
-  gif_createImg = createImg("assets/gifboogie.gif");
-}
+
 
 
 function setup() {
   // put setup code here
   createCanvas(1000,1000);
   background(0);
-  //myImage=loadImage();
+  B1=loadImage("assets/boogie1.png");
+  B2=loadImage("assets/boogie2.png");
 }
 
 function draw() {
   // put drawing code here
-  image(gif_loadImg, 50, 50);
-  gif_createImg.position(50, 350);
+
+
   switch(myState){
 
     case 0:
-    image(myImage,0,0); //put in ever case it will show in
-    //put boogie in each case
-    break;
-
-    case 1:
+    image(B1,0,0,500,500);
+    textSize(30);
+    text("Wait for it",360,480);
     myTimer++;
     if (myTimer>200){
       myTimer=0;
-      myState=2;
+      myState=1;
     }
+
+    break;
+
+    case 1:
+    image(B2,0,0,500,500);
+    textSize(30);
+    text("Click",360,480);
+
+
     break;
 
     case 2:
+    image(B2,0,0,500,500);
+    fill(255, 235, 10);
+    ellipse(479,258,50,50);
+    textSize(30);
+    text("Click",360,480);
     break;
 
     case 3:
+    image(B2,0,0,500,500);
+    fill(255, 235, 10);
+    ellipse(479,258,50,50);
+    textSize(25);
+    text("Mr.Boogie doesn't like light",63,y);
+    y=y+1;
+    if (y>40){
+      myState=4;
+    }
     break;
 
     case 4:
+    image(B2,0,0,500,500);
+    fill(255, 235, 10);
+    ellipse(479,258,50,50);
+    textSize(25);
+    text("Mr.Boogie doesn't like light",63,y);
     break;
 
   }
@@ -49,7 +74,10 @@ function draw() {
 function mouseReleased(){
 console.log(mouseX + ',' + mouseY);
 
-  if (myState==0) {
-    myState=1;
+
+  myState++;
+  if (myState>4){
+    myState=0;
+    y=-10;
   }
 }
