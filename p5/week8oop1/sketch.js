2d1d8c302ce10914718a32d70a9e8668edfb7e53
Brooke <BAMAST1@ilstu.edu>
2019-10-10 16:43:28 -0700

    week8assingment

---
+++ p5/week8oop1/sketch.js
@@ -0,0 +1,49 @@
+var myCar1; //declair it
+var myCar2;
+var myCar3;
+
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  myCar1 = new Car(); //spawn 1 car
+  myCar2 = new Car();
+  myCar3= new Car();
+}
+
+function draw() {
+  // put drawing code here
+  background(100);
+  myCar1.display();
+  myCar1.move();
+
+  myCar2.display();
+  myCar2.move();
+
+  myCar3.display();
+  myCar3.move();
+
+}
+
+function Car() {
+  //attributes
+  //methods
+  this.xPos = 100;
+
+  }
+
+
+
+
+  this.display = function() {
+    fill(this.r,this.g,this.b);
+    rect(this.xPose=this.xPose+1);
+  }
+
+  this.move = function() {
+    this.xPos = this.xPos + 1;
+
+  }
+
+
+}
---
+++ p5/week8random/sketch.js
@@ -0,0 +1,22 @@
+var bands = ['pvris', 'bring me the horizon', 'tool', 'korn', 'a perfect circle', 'iron and wine'] ;
+var aRandomBand = '' ;
+
+function setup() {
+  createCanvas(800, 800) ;
+  textAlign(CENTER, CENTER) ;
+}
+
+function draw() {
+  background(100) ;
+  textSize(48);
+  text(aRandomBand, width/2, height/2) ;
+}
+
+function mouseClicked() {
+
+  var i = floor(random(bands.length)) ;  // floor changes the random to an integer
+
+  aRandomBand = bands[i] ;
+
+
+}
